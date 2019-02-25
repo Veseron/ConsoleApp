b3c7f037ad4ad94ad783280c945163e89595ff1f
Alenww <43764167+Alenww@users.noreply.github.com>
2019-02-25 12:50:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace ConsoleApp
             Console.WriteLine("Developer: Mikhail Shumakov");
             Console.WriteLine("Developer: Anastasia Skrynnikova");
             Console.WriteLine("Developer: Vladimir Batiyk");
-
+            Console.WriteLine("Developer: Klimenko Alena");
             Console.ReadKey();
         }
     }
