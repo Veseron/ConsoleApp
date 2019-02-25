5f7cb6b004f7a573a8f559cee6fe26bb715258ff
NadyaMojelskaya <47971872+NadyaMojelskaya@users.noreply.github.com>
2019-02-25 12:49:59 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,6 +15,7 @@ namespace ConsoleApp
             Console.WriteLine("Developer: Mikhail Shumakov");
             Console.WriteLine("Developer: Anastasia Skrynnikova");
             Console.WriteLine("Developer: Vladimir Batiyk");
+            Console.WriteLine("Developer: Nadya Mojelskaya");
 
             Console.ReadKey();
         }
